32cb7ef3c9accb219fda4a9dc6f30504ce39f0ca
Mikkel Grønning <5516569-MikkelGroenning@users.noreply.gitlab.com>
2020-11-10 10:20:09 +0100

    Update environment

--- data_prep/get_tweets.py
+++
@@ -1,8 +0,0 @@
-from ..twitter_api import auth
-
-import pandas as pd
-import tweepy
-from tqdm import trange
-
-#%%
-
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='TEST_ENVIROMENT',
+    author='Mikkel Grønning',
+    license='MIT',
+)
+
+
+some_dict = {
+"user" : "marco", "address" : "some street"
+,
+ "age" : 28
+ 
+ }
