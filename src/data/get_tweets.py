ed9239ed0c056509147829305ab1743448bffb4b
Mikkel Grønning <5516569-MikkelGroenning@users.noreply.gitlab.com>
2020-11-11 10:58:11 +0100

    get media tweets

--- src/data/get_tweets.py
+++ src/data/get_tweets.py
@@ -10,66 +10,21 @@ from src.tools.twitter_api import auth
 api = tweepy.API(auth, wait_on_rate_limit=True)
 #%%
 
-# screen name of the account to be fetched
-userIDs = ["CNN", "foxnews", "BreitbartNews"]
-end_date = datetime.datetime(2020, 1, 3, 0, 0, 0)
 
-for userID in userIDs:
-    print(userID)
-    # fetching the statuses
-    tweets = api.user_timeline(
-        screen_name=userID,
-        # 200 is the maximum allowed count
-        count=200,
-        include_rts=False,
-        # Necessary to keep full_text
-        # otherwise only the first 140 words are extracted
-        tweet_mode="extended",
-    )
+#%%
 
-    all_tweets = []
-    all_tweets.extend(tweets)
-    oldest_id = tweets[-1].id
+results = []
 
-    # exstract all tweets since the ea
-    while tweets[-1].created_at > end_date:
-        tweets = api.user_timeline(
-            screen_name=userID,
-            # 200 is the maximum allowed count
-            count=200,
-            include_rts=False,
-            max_id=oldest_id - 1,
-            # Necessary to keep full_text
-            # otherwise only the first 140 words are extracted
-            tweet_mode="extended",
-        )
-        if len(tweets) == 0:
-            break
-        oldest_id = tweets[-1].id
-        all_tweets.extend(tweets)
+for i in trange(len(df)//100):
 
-        print("N of tweets downloaded till now {}".format(len(all_tweets)))
-# %%
+    ids = list(df["tweet_id"].iloc[i*100:i*100+100])
+    response = [
+            (t.id, t.created_at, t.text) 
+            for t in api.statuses_lookup(id_=ids)]
 
-tweets 
+    results.extend(response)
 
-# %%
-userID = "peterfalktoft"
-tweets = api.user_timeline(
-        screen_name=userID,
-        # 200 is the maximum allowed count
-        count=200,
-        include_rts=False,
-        max_id=1246490680052125707 - 1,
-        # Necessary to keep full_text
-        # otherwise only the first 140 words are extracted
-        tweet_mode="extended",
-    )
-
-
-
-
-# %%
-tweets[0]
-
-# %%
+#%%
+tweets_200316 = pd.DataFrame(
+    results, columns=['tweet_id', 'date_time', 'tweet']
+)
