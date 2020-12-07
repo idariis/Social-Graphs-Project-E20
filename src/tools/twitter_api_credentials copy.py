d8998bdf3e0e236b23daab40028efd29e6db76d8
Mikkel Grønning <MikkelGroenning@users.noreply.github.com>
2020-12-07 10:54:23 +0100

    Update twitter_api_credentials copy.py

--- src/tools/twitter_api_credentials copy.py
+++ src/tools/twitter_api_credentials copy.py
@@ -1,11 +1,11 @@
 import tweepy
 
-api_key = "vhpYxpHap3JT2knXp3hyiUrJs"
-api_secret_key = "TVsN2J4ZrHEHwrPlhtihuBWolXWibF7878exrK9hQ7zQywq9Dp"
+api_key = ""
+api_secret_key = ""
 
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAGe7JQEAAAAAxY1bj4PHloQmCG6MXMGiKVoUlyM%3DwLgIydkepC59w4RJ54ghNEuznPUDQkC2yhwr1deQCwwQY02V5L"
-access_token = "1324015519708684289-HBCPYHbS8HPZ0Rq4YY5ZarJvOmei8I"
-access_token_secret = "6NO0tM6dMoapFyCLLFt2OaQJ1xmvO1sUoUmrPJdmmquXx"
+bearer_token = ""
+access_token = ""
+access_token_secret = ""
 
 auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, access_token_secret)
